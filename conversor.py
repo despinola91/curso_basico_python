fe468a573282c4dadbc2e128d8531b6525ceffa9
Dario Espinola <despinola@request.com.ar>
2020-08-16 12:28:34 -0300

    adding function to calculate dollars

--- conversor.py
+++ conversor.py
@@ -1,3 +1,12 @@
+def calcular_dolares(valor_dolar, tipo_peso):
+    pesos = input("Cuantos pesos " + tipo_peso + " tienes?: ")
+    pesos = float(pesos)
+    #valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+
 menu = """
 Bienvenido al conversor de monedas 💰
 
@@ -10,28 +19,10 @@ Elige una opción: """
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = input("Cuantos pesos colombianos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 3875
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dólares")
+    calcular_dolares(3875, "colombianos")
 elif opcion == 2:
-    pesos = input("Cuantos pesos argentinos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 65
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dólares")
+    calcular_dolares(65, "argentinos")
 elif opcion == 3:
-    pesos = input("Cuantos pesos mexicanos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 24
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dólares")
+    calcular_dolares(24, "mexicanos")
 else:
     print("Ingresa una opción correcta por favor.")
