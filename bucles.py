327ee996e73c4f3787b57a422ad6e01f98b627fa
Dario Espinola <despinola@request.com.ar>
2020-08-16 15:28:40 -0300

    improving message

--- bucles.py
+++ bucles.py
@@ -4,7 +4,7 @@ def run():
     potencia_2 = 2**contador
 
     while potencia_2 < LIMITE:
-        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
         contador = contador + 1
         potencia_2 = 2**contador
 
