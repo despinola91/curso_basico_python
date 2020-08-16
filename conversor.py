4c4a3904f15ed9a5424c008876821ccfbca7b0f0
Dario Espinola <despinola@request.com.ar>
2020-08-16 12:02:59 -0300

    adding different countries as options

--- conversor.py
+++ conversor.py
@@ -1,12 +1,37 @@
-pesos = input("¿Cuantos pesos argentinos tienes?: ")
-pesos = float(pesos)
+menu = """
+Bienvenido al conversor de monedas 💰
 
-valor_dolar = input("¿Cuanto es el tipo de cambio de pesos a dolares?: ")
-valor_dolar = float(valor_dolar)
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
 
-#valor_dolar = 3875
+Elige una opción: """
 
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes USD " + dolares + " dólares.")
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("Cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("Cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print("Ingresa una opción correcta por favor.")
